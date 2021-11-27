611ed2422af22a569580205850d5fea19d0017f5
Carlos Saltos <csaltos@gmail.com>
2021-11-27 15:19:48 +0200

    Versioning Docker build images

--- build-mesos.sh
+++ build-mesos.sh
@@ -2,8 +2,8 @@
 
 BASE_DIR=$(dirname $0)
 
-BUILD_IMAGE_NAME=debian-buster
-MESOS_VERSION=HEAD
+BUILD_IMAGE_NAME=${1:-debian-buster}
+MESOS_VERSION=${2:-HEAD}
 
 CACHE_BASE=$BASE_DIR/cache
 CACHE_MESOS_REPO=$CACHE_BASE/mesos
@@ -24,9 +24,6 @@ TARGET_BASE=$BASE_DIR/target
 TARGET_IMAGE=$TARGET_BASE/$BUILD_IMAGE_NAME
 TARGET_MESOS_REPO=$TARGET_IMAGE/mesos-repo
 
-OUTPUT=$BASE_DIR/output
-OUTPUT_PACKAGE=$OUTPUT/mesos-$MESOS_LABEL_VERSION-$BUILD_IMAGE_NAME.deb
-
 rm -rf $TARGET_BASE
 mkdir -p $TARGET_BASE
 
@@ -39,14 +36,17 @@ cp -a $CACHE_MESOS_REPO $TARGET_MESOS_REPO
 
 if [[ $MESOS_VERSION == HEAD ]]; then
     git -C $TARGET_MESOS_REPO checkout master
+    MESOS_LABEL_VERSION=$(grep "AC_INIT..mesos.," $TARGET_MESOS_REPO/configure.ac | sed "s/[^0-9.]//g")
 else
     git -C $TARGET_MESOS_REPO checkout $MESOS_VERSION
+    MESOS_LABEL_VERSION=$MESOS_VERSION
 fi
 
-MESOS_LABEL_VERSION=$(grep "AC_INIT..mesos.," $TARGET_MESOS_REPO/configure.ac | sed "s/[^0-9.]//g")
+OUTPUT=$BASE_DIR/output
+OUTPUT_PACKAGE=$OUTPUT/mesos-$MESOS_LABEL_VERSION-$BUILD_IMAGE_NAME.deb
 
-docker build -t mesos-build-$BUILD_IMAGE_NAME $TARGET_IMAGE
+docker build -t mesos-build-$BUILD_IMAGE_NAME:$MESOS_LABEL_VERSION $TARGET_IMAGE
 
-docker run -it mesos-build-debian-buster:latest cat /builder/mesos-deb-packaging/pkg.deb > $OUTPUT_PACKAGE
+docker run mesos-build-$BUILD_IMAGE_NAME:$MESOS_LABEL_VERSION cat /builder/mesos-deb-packaging/pkg.deb > $OUTPUT_PACKAGE
 
 echo "Apache Mesos $MESOS_LABEL_VERSION is at $OUTPUT_PACKAGE"
