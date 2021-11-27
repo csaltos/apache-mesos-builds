79fa5cd9e8fcfa93e151d43c2fd15ba974e6fadb
Carlos Saltos <csaltos@gmail.com>
2021-11-27 10:08:35 +0200

    Detecting HEAD version

--- build-mesos-debian-buster.sh
+++ build-mesos-debian-buster.sh
@@ -1,25 +1,30 @@
 #!/bin/bash -e
 
+BASE_DIR=$(dirname $0)
+
 BUILD_IMAGE_NAME=debian-buster
 MESOS_VERSION=HEAD
 
-CACHE_BASE=$(dirname $0)/cache
+CACHE_BASE=$BASE_DIR/cache
 CACHE_MESOS_REPO=$CACHE_BASE/mesos
 
+mkdir -p $CACHE_BASE
 if [[ ! -d $CACHE_MESOS_REPO ]]; then
     git -C $CACHE_BASE clone https://gitbox.apache.org/repos/asf/mesos.git
 fi
 git -C $CACHE_MESOS_REPO pull
 
-BUILDER_BASE=$(dirname $0)/builder
+BUILDER_BASE=$BASE_DIR/builder
 BUILDER_IMAGE_BASE=$BUILDER_BASE/mesos-docker-build-images
 BUILDER_IMAGE=$BUILDER_IMAGE_BASE/$BUILD_IMAGE_NAME
 BUILDER_INSTALLERS=$BUILDER_IMAGE_BASE/installers
 BUILDER_SCRIPTS=$BUILDER_IMAGE/scripts
 
-TARGET_BASE=$(dirname $0)/target
+TARGET_BASE=$BASE_DIR/target
 TARGET_IMAGE=$TARGET_BASE/$BUILD_IMAGE_NAME
 TARGET_MESOS_REPO=$TARGET_IMAGE/mesos-repo
+
+OUTPUT=$BASE_DIR/output
 
 rm -rf $TARGET_BASE
 mkdir -p $TARGET_BASE
@@ -34,9 +39,11 @@ cp -a $CACHE_MESOS_REPO $TARGET_MESOS_REPO
 if [[ $MESOS_VERSION == HEAD ]]; then
     git -C $TARGET_MESOS_REPO checkout master
 else
-    git -C $TARGET_MESOS_REPO checkout tag/$MESOS_VERSION
+    git -C $TARGET_MESOS_REPO checkout $MESOS_VERSION
 fi
+
+MESOS_LABEL_VERSION=$(grep "AC_INIT..mesos.," $TARGET_MESOS_REPO/configure.ac | sed "s/[^0-9.]//g")
 
 docker build -t mesos-build-$BUILD_IMAGE_NAME $TARGET_IMAGE
 
-docker run -it mesos-build-debian-buster:latest cp /builder/mesos-deb-packaging/pkg.deb $TARGET/mesos-$MESOS_VERSION-$BUILD_IMAGE_NAME.deb
+docker run -it mesos-build-debian-buster:latest cat /builder/mesos-deb-packaging/pkg.deb > $OUTPUT/mesos-$MESOS_LABEL_VERSION-$BUILD_IMAGE_NAME.deb
